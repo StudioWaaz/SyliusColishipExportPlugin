c55fceb338d4a25dd2fb29770707ef05101b8322
orkatz <orkatz@studiowaaz.com>
2025-04-07 11:53:14 +0200

    update dependence in composer.json and update flash_bag by request_stack

--- src/Api/ShippingLabelFetcher.php
+++ src/Api/ShippingLabelFetcher.php
@@ -5,17 +5,17 @@ declare(strict_types=1);
 namespace Ikuzo\SyliusColishipPlugin\Api;
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Riverline\MultiPartParser\StreamedPart;
 
 
 class ShippingLabelFetcher implements ShippingLabelFetcherInterface
 {
-    private FlashBagInterface $flashBag;
+    private RequestStack $requestStack;
 
-    public function __construct(FlashBagInterface $flashBag, WebClientInterface $webClient, SoapClientInterface $soapClient)
+    public function __construct(RequestStack $requestStack, WebClientInterface $webClient, SoapClientInterface $soapClient)
     {
-        $this->flashBag = $flashBag;
+        $this->requestStack = $requestStack;
         $this->webClient = $webClient;
         $this->soapClient = $soapClient;
     }
@@ -29,12 +29,12 @@ class ShippingLabelFetcher implements ShippingLabelFetcherInterface
             $requestData = $this->webClient->getRequestData($weight);
 
             $this->response = $this->soapClient->createShipment($requestData, $weight);
-            
+
             if ($this->response->return->messages->type == "ERROR") {
                 throw new \Exception($this->response->return->messages->messageContent, 1);
             }
         } catch (\SoapFault $exception) {
-            $this->flashBag->add(
+            $this->getSession()->getFlashBag()->add(
                 'error',
                 sprintf(
                     'Colissimo Service for #%s order: %s',
@@ -50,11 +50,12 @@ class ShippingLabelFetcher implements ShippingLabelFetcherInterface
     public function getLabelContent(): ?array
     {
         if (!isset($this->response->return->labelV2Response->label)) {
+
             $this->flashBag->add('error', $this->response->return->messages->messageContent);
             return null;
         }
+        $this->getSession()->getFlashBag()->add('success', 'bitbag.ui.shipment_data_has_been_exported');
 
-        $this->flashBag->add('success', 'bitbag.ui.shipment_data_has_been_exported');
 
         return [
             'parcelNumber' => $this->response->return->labelV2Response->parcelNumber,
