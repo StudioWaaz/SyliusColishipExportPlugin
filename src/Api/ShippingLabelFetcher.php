c06f89dda681e9d083e8d9236db0be6fdeb07b72
orkatz <orkatz@studiowaaz.com>
2025-04-15 10:13:14 +0200

    update requestStack

--- src/Api/ShippingLabelFetcher.php
+++ src/Api/ShippingLabelFetcher.php
@@ -4,17 +4,21 @@ declare(strict_types=1);
 
 namespace Ikuzo\SyliusColishipPlugin\Api;
 
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Riverline\MultiPartParser\StreamedPart;
-
 
 class ShippingLabelFetcher implements ShippingLabelFetcherInterface
 {
     private RequestStack $requestStack;
+    private WebClientInterface $webClient;
+    private SoapClientInterface $soapClient;
 
-    public function __construct(RequestStack $requestStack, WebClientInterface $webClient, SoapClientInterface $soapClient)
-    {
+    public function __construct(
+        RequestStack $requestStack,
+        WebClientInterface $webClient,
+        SoapClientInterface $soapClient
+    ) {
         $this->requestStack = $requestStack;
         $this->webClient = $webClient;
         $this->soapClient = $soapClient;
@@ -34,36 +38,36 @@ class ShippingLabelFetcher implements ShippingLabelFetcherInterface
                 throw new \Exception($this->response->return->messages->messageContent, 1);
             }
         } catch (\SoapFault $exception) {
-            $this->getSession()->getFlashBag()->add(
-                'error',
-                sprintf(
-                    'Colissimo Service for #%s order: %s',
-                    $shipment->getOrder()->getNumber(),
-                    $exception->getMessage()
-                )
-            );
+            $request = $this->requestStack->getCurrentRequest();
+            if ($request !== null) {
+                $request->getSession()->getFlashBag()->add(
+                    'error',
+                    sprintf(
+                        'Colissimo Service for #%s order: %s',
+                        $shipment->getOrder()->getNumber(),
+                        $exception->getMessage()
+                    )
+                );
+            }
         }
-
-        return;
     }
 
     public function getLabelContent(): ?array
     {
-        if (!isset($this->response->return->labelV2Response->label)) {
+        $request = $this->requestStack->getCurrentRequest();
+        if ($request !== null) {
+            if (!isset($this->response->return->labelV2Response->label)) {
+                $request->getSession()->getFlashBag()->add('error', $this->response->return->messages->messageContent);
+                return null;
+            }
 
-            $this->flashBag->add('error', $this->response->return->messages->messageContent);
-            return null;
+            $request->getSession()->getFlashBag()->add('success', 'bitbag.ui.shipment_data_has_been_exported');
         }
-        $this->getSession()->getFlashBag()->add('success', 'bitbag.ui.shipment_data_has_been_exported');
-
 
         return [
             'parcelNumber' => $this->response->return->labelV2Response->parcelNumber,
             'label' => $this->response->return->labelV2Response->label,
             'cn23' => (isset($this->response->return->labelV2Response->cn23)) ? $this->response->return->labelV2Response->cn23 : null
         ];
-
-        // return $this->response->return->labelV2Response->label;
     }
-
 }
